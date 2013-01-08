021c4a7342032e65fd02a7d33da402c14a2c430d
Thạch Tùng Anh <thachtunganh@gmail.com>
2013-01-08 22:05:59 +0700

    Clean up the source.

--- usr/.initramfs_data.cpio.cmd
+++
@@ -1,1 +0,0 @@
-cmd_usr/initramfs_data.cpio := /bin/bash /home/chun/chun/a/scripts/gen_initramfs_list.sh -o usr/initramfs_data.cpio   -d
--- usr/.initramfs_data.cpio.d
+++
@@ -1,1 +0,0 @@
-deps_initramfs := /home/chun/chun/a/scripts/gen_initramfs_list.sh \
