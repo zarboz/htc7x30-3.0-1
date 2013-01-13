e9134e60110c3d0735b9132ef8ca9fe24ce7905c
Thạch Tùng Anh <thachtunganh@Gmail.com>
2013-01-13 12:38:58 +0700

    Clean up the kernel source, prepare for .gitignore.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 4 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Tue Jan 8 22:15:30 ICT 2013"
-#define LINUX_COMPILE_BY "thachtunganh"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.7.3 20121205 (prerelease) (Linaro GCC 4.7-2012.12) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.56-Advanced-ICS-Sense-Kernel"
